92204884c1689b1973c82611127ac689c6aee2b7
OBRISON <vjekili@gmail.com>
2024-06-14 15:56:56 +0200

    Update steam.js

--- js/steam.js
+++ js/steam.js
@@ -1,25 +1,23 @@
-var totalfilez
+var totalfilez;
 //Gets initial total files needed.
 function SetFilesTotal(total) {
     totalfilez = total;
     window.totalfiles = total;
 }
+
 //Update Progress Bar
 function SetFilesNeeded(needed) {
     window.filesleft = needed;
-    if(needed < 1) {
-        var neededz = window.totalfiles;
-    } else {
-        var neededz = needed;
-    }
-    var percent = Math.ceil((((window.totalfiles-neededz)/2)/(window.totalfiles/2))*100);
-    if(!isNan(percent)) {
-        $('#progressbar').css({ "width" : percent+"%"});
-        $('#progressbar').empty().append(percent+"%");
+    var neededz = needed < 1 ? window.totalfiles : needed;
+    var percent = Math.ceil((((window.totalfiles - neededz) / 2) / (window.totalfiles / 2)) * 100);
+    if (!isNaN(percent)) {
+        $('#progressbar').css({ "width": percent + "%" });
+        $('#progressbar').empty().append(percent + "%");
     }
 }
+
 //Update loading header text.
-function SetStatusChanged( status ) {
+function SetStatusChanged(status) {
     /*
     Retrieving server info...
     Getting addon info for #------
@@ -28,24 +26,25 @@ function SetStatusChanged( status ) {
     Workshop Complete
     Sending client info...
     */
-    if(status == 'Retrieving server info...') {
-        $('#progressbar').css({ "width" : "10%"});
+    if (status == 'Retrieving server info...') {
+        $('#progressbar').css({ "width": "10%" });
         $('#subtext1').empty().append("Initializing...");
     }
-    if(status == 'Mounting Addons') {
-        $('#progressbar').css({ "width" : "50%"});
+    if (status == 'Mounting Addons') {
+        $('#progressbar').css({ "width": "50%" });
         $('#subtext1').empty().append("Mounting Addons");
     }
-    if(status == 'Workshop Complete') {
-        $('#progressbar').css({ "width" : "80%"});
+    if (status == 'Workshop Complete') {
+        $('#progressbar').css({ "width": "80%" });
         $('#subtext1').empty().append("Workshop Complete");
     }
-    if(status == 'Sending client info...') {
-        $('#progressbar').css({"width" : "100%"}, 15000);
+    if (status == 'Sending client info...') {
+        $('#progressbar').css({ "width": "100%" }, 15000);
         $('#subtext1').empty().append('Finalizing...');
     }
 }
+
 //Downloading file event.
 function DownloadingFile(fileName) {
-    $('#subtext1').empty().append('Obtaining '+fileName+'.</br><b>'+window.filesleft+'</b> out of <b>'+window.totalfiles+'</b>');
+    $('#subtext1').empty().append('Obtaining ' + fileName + '.</br><b>' + window.filesleft + '</b> out of <b>' + window.totalfiles + '</b>');
 }
